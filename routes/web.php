ee4103ca7b869cd91742922d40f1866b601d53f6
tirkeshdev <tirkesgeldigarayew@gmail.com>
2024-07-02 17:05:41 +0500

    2.1.0

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SellerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('index');
+Route::get('/category/{id}', [ProductController::class, 'index'])->name('index');
+Route::get('/seller/{id}', [SellerController::class, 'show'])->name('seller_show');
+Route::get('/product/{id}', [ProductController::class, 'show'])->name('show');
+Route::get('/discounts', [ProductController::class, 'discounts'])->name('discounts');
+Route::get('/builts', [ProductController::class, 'builts'])->name('builts');
+Route::get('/locale/{locale}', [HomeController::class, 'locale'])->name('locale')->whereIn('locale', [ 'ru', 'en']);
